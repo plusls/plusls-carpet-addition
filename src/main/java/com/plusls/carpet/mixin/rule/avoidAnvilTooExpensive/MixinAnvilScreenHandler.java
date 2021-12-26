1dd31f15413ed6138f5aba003236ba0adf959c54
plusls <plusls@qq.com>
2021-12-26 17:13:53 +0800

    fix #14

--- src/main/java/com/plusls/carpet/mixin/rule/avoidAnvilTooExpensive/MixinAnvilScreenHandler.java
+++ src/main/java/com/plusls/carpet/mixin/rule/avoidAnvilTooExpensive/MixinAnvilScreenHandler.java
@@ -1,20 +1,49 @@
 package com.plusls.carpet.mixin.rule.avoidAnvilTooExpensive;
 
 import com.plusls.carpet.PcaSettings;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.item.ItemStack;
 import net.minecraft.screen.AnvilScreenHandler;
-import net.minecraft.screen.Property;
+import net.minecraft.screen.ForgingScreenHandler;
+import net.minecraft.screen.ScreenHandlerContext;
+import net.minecraft.screen.ScreenHandlerType;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.logging.log4j.Logger;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Slice;
 
 @Mixin(AnvilScreenHandler.class)
-public class MixinAnvilScreenHandler {
-    @Redirect(method = "updateResult", at = @At(value = "INVOKE", target = "Lnet/minecraft/screen/Property;get()I", ordinal = 1))
-    private int redirectGet(Property property) {
-        if (PcaSettings.avoidAnvilTooExpensive) {
-            return 0;
-        } else {
-            return property.get();
+public abstract class MixinAnvilScreenHandler extends ForgingScreenHandler {
+    @Shadow
+    private String newItemName;
+
+
+    @Shadow
+    @Final
+    private static Logger LOGGER;
+
+    public MixinAnvilScreenHandler(@Nullable ScreenHandlerType<?> type, int syncId, PlayerInventory playerInventory, ScreenHandlerContext context) {
+        super(type, syncId, playerInventory, context);
+    }
+
+    @ModifyVariable(method = "updateResult",
+            slice = @Slice(from = @At(value = "INVOKE", target = "Lnet/minecraft/screen/Property;get()I", ordinal = 1)),
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;isEmpty()Z", ordinal = 0), ordinal = 1)
+    private ItemStack setItemStack(ItemStack itemStack) {
+        ItemStack inputStack = this.input.getStack(0);
+        if (PcaSettings.avoidAnvilTooExpensive && itemStack.isEmpty() && !inputStack.isEmpty() &&
+                (this.input.getStack(1) != ItemStack.EMPTY ||
+                        (StringUtils.isBlank(this.newItemName) && inputStack.hasCustomName()) ||
+                        (!StringUtils.isBlank(this.newItemName) && !this.newItemName.equals(inputStack.getName().getString()))))
+        {
+            return inputStack.copy();
+        } else{
+            return itemStack;
         }
     }
 }
