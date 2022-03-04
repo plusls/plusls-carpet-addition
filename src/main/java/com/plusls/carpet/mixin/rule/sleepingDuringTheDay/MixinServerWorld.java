8e41bb1216656570fd56a391081a9d101e143b55
plusls <plusls@qq.com>
2022-03-04 12:58:32 +0800

    update to 1.18.2

--- src/main/java/com/plusls/carpet/mixin/rule/sleepingDuringTheDay/MixinServerWorld.java
+++ src/main/java/com/plusls/carpet/mixin/rule/sleepingDuringTheDay/MixinServerWorld.java
@@ -3,6 +3,7 @@ package com.plusls.carpet.mixin.rule.sleepingDuringTheDay;
 import com.plusls.carpet.PcaSettings;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.profiler.Profiler;
+import net.minecraft.util.registry.RegistryEntry;
 import net.minecraft.util.registry.RegistryKey;
 import net.minecraft.world.MutableWorldProperties;
 import net.minecraft.world.StructureWorldAccess;
@@ -16,8 +17,9 @@ import java.util.function.Supplier;
 
 @Mixin(ServerWorld.class)
 public abstract class MixinServerWorld extends World implements StructureWorldAccess {
-    protected MixinServerWorld(MutableWorldProperties properties, RegistryKey<World> registryRef, DimensionType dimensionType, Supplier<Profiler> profiler, boolean isClient, boolean debugWorld, long seed) {
-        super(properties, registryRef, dimensionType, profiler, isClient, debugWorld, seed);
+
+    protected MixinServerWorld(MutableWorldProperties properties, RegistryKey<World> registryRef, RegistryEntry<DimensionType> registryEntry, Supplier<Profiler> profiler, boolean isClient, boolean debugWorld, long seed) {
+        super(properties, registryRef, registryEntry, profiler, isClient, debugWorld, seed);
     }
 
     // 根据当前时间设置夜晚和白天
--- src/main/java/com/plusls/carpet/mixin/rule/spawnBiome/MixinSpawnHelper.java
+++ src/main/java/com/plusls/carpet/mixin/rule/spawnBiome/MixinSpawnHelper.java
@@ -1,38 +1,32 @@
 package com.plusls.carpet.mixin.rule.spawnBiome;
 
 import com.plusls.carpet.PcaSettings;
-import net.minecraft.entity.SpawnGroup;
-import net.minecraft.util.collection.Pool;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.registry.BuiltinRegistries;
+import net.minecraft.util.registry.RegistryEntry;
 import net.minecraft.world.SpawnHelper;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.biome.BiomeKeys;
-import net.minecraft.world.biome.SpawnSettings;
-import net.minecraft.world.gen.StructureAccessor;
-import net.minecraft.world.gen.chunk.ChunkGenerator;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(SpawnHelper.class)
 public class MixinSpawnHelper {
-    @Redirect(method = "getSpawnEntries",
-            at = @At(value = "INVOKE",
-                    target = "Lnet/minecraft/world/gen/chunk/ChunkGenerator;getEntitySpawnList(Lnet/minecraft/world/biome/Biome;Lnet/minecraft/world/gen/StructureAccessor;Lnet/minecraft/entity/SpawnGroup;Lnet/minecraft/util/math/BlockPos;)Lnet/minecraft/util/collection/Pool;"))
-    private static Pool<SpawnSettings.SpawnEntry> modifyBiome(ChunkGenerator chunkGenerator, Biome biome, StructureAccessor accessor, SpawnGroup group, BlockPos pos) {
+    @ModifyVariable(method = "getSpawnEntries",
+            at = @At(value = "HEAD"), ordinal = 0, argsOnly = true)
+    private static RegistryEntry<Biome> modifyBiome(RegistryEntry<Biome> biomeEntry) {
         if (PcaSettings.spawnBiome != PcaSettings.PCA_SPAWN_BIOME.DEFAULT) {
             if (PcaSettings.spawnBiome == PcaSettings.PCA_SPAWN_BIOME.DESERT) {
-                biome = BuiltinRegistries.BIOME.get(BiomeKeys.DESERT);
+                biomeEntry = RegistryEntry.of(BuiltinRegistries.BIOME.get(BiomeKeys.DESERT));
             } else if (PcaSettings.spawnBiome == PcaSettings.PCA_SPAWN_BIOME.PLAINS) {
                 // BuiltinBiomes
-                biome = BuiltinRegistries.BIOME.get(BiomeKeys.PLAINS);
+                biomeEntry = RegistryEntry.of(BuiltinRegistries.BIOME.get(BiomeKeys.PLAINS));
             } else if (PcaSettings.spawnBiome == PcaSettings.PCA_SPAWN_BIOME.THE_END) {
-                biome = BuiltinRegistries.BIOME.get(BiomeKeys.THE_END);
+                biomeEntry = RegistryEntry.of(BuiltinRegistries.BIOME.get(BiomeKeys.THE_END));
             } else if (PcaSettings.spawnBiome == PcaSettings.PCA_SPAWN_BIOME.NETHER_WASTES) {
-                biome = BuiltinRegistries.BIOME.get(BiomeKeys.NETHER_WASTES);
+                biomeEntry = RegistryEntry.of(BuiltinRegistries.BIOME.get(BiomeKeys.NETHER_WASTES));
             }
         }
-        return chunkGenerator.getEntitySpawnList(biome, accessor, group, pos);
+        return biomeEntry;
     }
 }
