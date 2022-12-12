8596b6816d4d50454bfaeb7a9ea5715544e8f119
Hendrix-Shen <HendrixShen@hendrixshen.top>
2022-12-12 15:42:26 +0800

    Update to 1.19.3

--- src/main/java/com/plusls/carpet/mixin/rule/spawnBiome/MixinSpawnHelper.java
+++ src/main/java/com/plusls/carpet/mixin/rule/spawnBiome/MixinSpawnHelper.java
@@ -1,8 +1,8 @@
 package com.plusls.carpet.mixin.rule.spawnBiome;
 
 import com.plusls.carpet.PcaSettings;
-import net.minecraft.util.registry.BuiltinRegistries;
-import net.minecraft.util.registry.RegistryEntry;
+import net.minecraft.registry.*;
+import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.world.SpawnHelper;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.biome.BiomeKeys;
@@ -12,19 +12,21 @@ import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(SpawnHelper.class)
 public class MixinSpawnHelper {
+    private static final RegistryEntryLookup<Biome> pca$lookup = BuiltinRegistries.createWrapperLookup().createRegistryLookup().getOrThrow(RegistryKeys.BIOME);
+
     @ModifyVariable(method = "getSpawnEntries",
             at = @At(value = "HEAD"), ordinal = 0, argsOnly = true)
     private static RegistryEntry<Biome> modifyBiome(RegistryEntry<Biome> biomeEntry) {
         if (PcaSettings.spawnBiome != PcaSettings.PCA_SPAWN_BIOME.DEFAULT) {
+
             if (PcaSettings.spawnBiome == PcaSettings.PCA_SPAWN_BIOME.DESERT) {
-                biomeEntry = RegistryEntry.of(BuiltinRegistries.BIOME.get(BiomeKeys.DESERT));
+                biomeEntry = pca$lookup.getOrThrow(BiomeKeys.DESERT);
             } else if (PcaSettings.spawnBiome == PcaSettings.PCA_SPAWN_BIOME.PLAINS) {
-                // BuiltinBiomes
-                biomeEntry = RegistryEntry.of(BuiltinRegistries.BIOME.get(BiomeKeys.PLAINS));
+                biomeEntry = pca$lookup.getOrThrow(BiomeKeys.PLAINS);
             } else if (PcaSettings.spawnBiome == PcaSettings.PCA_SPAWN_BIOME.THE_END) {
-                biomeEntry = RegistryEntry.of(BuiltinRegistries.BIOME.get(BiomeKeys.THE_END));
+                biomeEntry = pca$lookup.getOrThrow(BiomeKeys.THE_END);
             } else if (PcaSettings.spawnBiome == PcaSettings.PCA_SPAWN_BIOME.NETHER_WASTES) {
-                biomeEntry = RegistryEntry.of(BuiltinRegistries.BIOME.get(BiomeKeys.NETHER_WASTES));
+                biomeEntry = pca$lookup.getOrThrow(BiomeKeys.NETHER_WASTES);
             }
         }
         return biomeEntry;
